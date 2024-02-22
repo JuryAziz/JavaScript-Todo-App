5582925478d1b52bcd6741ab3f452fbdaa6a61c5
Jury Alharbi <jury.abdulaziz@gmail.com>
2024-02-22 15:58:59 +0300

    loading from/to local storage

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,57 @@
-let tasks = [
-    { title: 'Display feature', checked: true },
-    { title: 'local storage', checked: false },
-    { title: 'add feature', checked: false }
-];
+let tasks;
+const tasksContainerElement = document.querySelector('.tasks-container'); // parent of all tasks
+const taskInput = document.querySelector('#task-title-input');
 
-const tasksContainerElement = document.querySelector(".tasks-container"); // parent of all tasks
-const taskInput = document.querySelector("#task-title-input");
+displayTasks = () => {
 
-displayTasks = (tasks) => {
+    // tasksContainerElement.replaceChildren();
+    loadFromLocalStorage();
+    tasks = [];
+
+    if (!tasks || tasks.length == 0) {
+        tasksContainerElement.textContent = 'NO TASKS';
+        return;
+    }
 
     tasks.forEach((task) => {
+        const taskElement = document.createElement('div');
+        const titleElement = document.createElement('p');
+        const checkElement = document.createElement('input');
 
-        const taskElement = document.createElement("div");
-        const titleElement = document.createElement("p");
-        const checkElement = document.createElement("input");
-
-        taskElement.className = "task";
-        titleElement.className = "title";
+        taskElement.className = 'task';
+        titleElement.className = 'title';
         titleElement.textContent = task.title;
-        checkElement.type = "checkbox";
+        checkElement.type = 'checkbox';
         checkElement.checked = task.checked;
-        
+
         taskElement.appendChild(checkElement);
         taskElement.appendChild(titleElement);
+        
+        // ? checkForChange() => button.onclick
+        // ? createTaskElement() => append to task container
         tasksContainerElement.appendChild(taskElement);
 
         // TODO: create and add delete button to task (event listener + append)
         // TODO: create and add edit button to task
         // TODO: show tasks count
     });
+    
+    loadToLocalStorage(tasks);
 };
 
-displayTasks(tasks);
+loadFromLocalStorage = () => {
+    try {
+        const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+        if (storedTasks) {
+            this.tasks = storedTasks;
+        }
+    } catch (ex) {
+        console.log('Sorry! an error occurred while fetching data from local storage');
+    }
+};
+
+loadToLocalStorage = (tasks) => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+displayTasks();
