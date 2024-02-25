552e699fabdd07b9dbc33e91b718cb5ee687e13a
Jury Alharbi <jury.abdulaziz@gmail.com>
2024-02-25 03:47:29 +0300

    styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,7 +12,7 @@ loadFromLocalStorage = () => {
             tasks = storedTasks;
         }
     } catch (ex) {
-        console.log(ex, 'Sorry! an error occurred while fetching data from local storage');
+        console.log('Sorry! an error occurred while fetching data from local storage');
     }
 }
 
@@ -24,61 +24,71 @@ renderTasks = (tasks) => {
     let checkedTasks = 0;
     tasksContainerElement.replaceChildren();
 
-    if (!tasks || tasks.length == 0) {
-        tasksContainerElement.textContent = 'NO TASKS';
+    const imgElement = document.createElement('img');
+    const textElement = document.createElement('p');
+    imgElement.classList = 'task-img';
+    imgElement.alt = 'cat status image';
+    if (tasks.length == 0) {
+
+        imgElement.src = './src/images/no-tasks.png';
+        textElement.textContent = 'we have no tasks';
+
+        tasksContainerElement.appendChild(imgElement);
+        tasksContainerElement.appendChild(textElement);
+
         return;
     }
+
+    tasksContainerElement.appendChild(imgElement);
+
+    tasks.sort((task1, task2) => task1.checked - task2.checked);
 
     tasks.forEach((task) => {
 
         createTaskElement(task);
-        task.checked ? checkedTasks++ : '';
+        task.checked ? checkedTasks++ : checkedTasks;
 
     });
 
-    const countElement = document.createElement('p');
-    countElement.textContent = 'remaining tasks:' + (tasks.length - checkedTasks);
-    tasksContainerElement.appendChild(countElement);
+    imgElement.src = checkedTasks != tasks.length ? './src/images/remaining-tasks.png' : './src/images/finished-tasks.png';
+    if (checkedTasks === 0) imgElement.src = './src/images/no-finished-tasks.png'
+
+    textElement.textContent = 'we have \'' + (tasks.length - checkedTasks) + '\' tasks remaining';
+    tasksContainerElement.appendChild(textElement);
 
 }
 
 addTask = () => {
-    try {
-        const taskTitle = taskInput.value.trim();
-        taskInput.value = '';
+    const taskTitle = taskInput.value.trim();
+    taskInput.value = '';
 
-        if (!taskTitle) return console.log('Oops no title specified');
-
-        const task = {
-            id: Date.now().toString(32),
-            title: taskTitle,
-            checked: false,
-        };
-
-        tasks.push(task);
-        loadToLocalStorage(tasks);
-        renderTasks(tasks);
-
-    } catch (ex) {
-        console.log(ex);
+    if (!taskTitle) {
+        popUp('You can\'t add a task with no title');
+        return;
     }
+
+    const task = {
+        id: Date.now().toString(32),
+        title: taskTitle,
+        checked: false,
+    };
+
+    tasks.push(task);
+    loadToLocalStorage(tasks);
+    renderTasks(tasks);
 }
 
 deleteTask = (id) => {
-    try {
-        tasks.splice(tasks.indexOf(tasks.find((task) => task.id === id)), 1);
-        loadToLocalStorage(tasks);
-        renderTasks(tasks);
-    } catch (error) {
-        console.log('Oops couldn\'t delete that!');
-    }
+    tasks.splice(tasks.indexOf(tasks.find((task) => task.id === id)), 1);
+    loadToLocalStorage(tasks);
+    renderTasks(tasks);
 }
 
 editTask = (id) => {
 
     const index = tasks.map(task => task.id).indexOf(id);
 
-    const taskElements = tasksContainerElement.children[index].children[0];
+    const taskElements = tasksContainerElement.children[index + 1].children[0];
 
     const editContainer = document.createElement('div');
     const newTitleInput = document.createElement('input');
@@ -86,11 +96,13 @@ editTask = (id) => {
 
     okButton.textContent = 'Ok';
     okButton.classList = 'btn';
-    editContainer.classList = 'task'
+
+    editContainer.classList = 'task-contents';
+    newTitleInput.classList = 'new-title-input';
 
     editContainer.appendChild(newTitleInput);
     editContainer.appendChild(okButton);
-    tasksContainerElement.children[index].appendChild(editContainer);
+    tasksContainerElement.children[index + 1].appendChild(editContainer);
 
     taskElements.style.display = 'none';
     newTitleInput.value = taskElements.children[1].textContent;
@@ -100,19 +112,13 @@ editTask = (id) => {
         let newTitle = newTitleInput.value.trim();
 
         if (!newTitle) {
-            if (confirm('you entered an empty title, do you want to delete the task?')) {
-                deleteTask(id);
-                return;
-            }
-
-            newTitle = taskElements.children[1].textContent;
-
+            popUp();
         }
-
         else {
             const checked = newTitle === taskElements.children[1].textContent;
+            const taskCheckValue = tasks.find((task) => task.id == id).checked;
             tasks.find((task) => task.id == id).title = newTitle;
-            tasks.find((task) => task.id == id).checked = checked;
+            tasks.find((task) => task.id == id).checked = checked ? taskCheckValue : false;
         }
 
         editContainer.style.display = 'none';
@@ -156,7 +162,7 @@ createTaskElement = (task) => {
 
     const taskElement = document.createElement('div');
     const taskItem = document.createElement('div');
-    const titleElement = document.createElement('p');
+    const titleElement = document.createElement('label');
     const checkElement = document.createElement('input');
     const deleteButtonElement = document.createElement('button');
     const editButtonElement = document.createElement('button');
@@ -171,7 +177,7 @@ createTaskElement = (task) => {
     taskElement.appendChild(taskItem);
     tasksContainerElement.appendChild(taskElement);
 
-    taskOptions(task, checkElement, deleteButtonElement, editButtonElement)
+    taskOptions(task, checkElement, deleteButtonElement, editButtonElement);
 
 }
 
@@ -180,19 +186,49 @@ styleTask = (task, taskElement, taskItem, checkElement, titleElement, deleteButt
     titleElement.textContent = task.title;
     checkElement.type = 'checkbox';
     checkElement.checked = task.checked;
-    deleteButtonElement.textContent = 'delete';
-    editButtonElement.textContent = 'edit';
+    deleteButtonElement.textContent = 'DEL';
+    editButtonElement.textContent = 'EDIT';
 
     taskElement.classList.add('task');
-    taskItem.classList.add('task');
+    taskItem.classList.add('task-contents');
     checkElement.classList.add('checkbox');
     titleElement.classList.add('task-title', checkElement.checked ? 'done-task' : 'todo-task');
     deleteButtonElement.classList.add('btn', 'delete-btn');
     editButtonElement.classList.add('btn', 'edit-btn');
 
 }
+
+popUp = () => {
+
+    const popUpContainer = document.createElement('div');
+    const popUpContent = document.createElement('div');
+    const popUpText = document.createElement('p');
+    const popUpImg = document.createElement('img');
+    const closeButton = document.createElement('button');
+
+    closeButton.textContent = 'CLOSE';
+    popUpText.textContent = 'how are you going to do a task that has no title?';
+    popUpImg.src = './src/images/no-title.png';
+
+    closeButton.classList.add('btn');
+    popUpImg.classList.add('img');
+    popUpContent.classList.add('popup-content');
+    popUpContainer.classList.add('popup-container');
+
+
+    popUpContent.appendChild(popUpText);
+    popUpContent.appendChild(popUpImg);
+    popUpContent.appendChild(closeButton);
+    popUpContainer.appendChild(popUpContent);
+
+    document.body.appendChild(popUpContainer);
+
+    closeButton.addEventListener('click', (ev) => {
+        document.body.removeChild(popUpContainer);
+    })
+
+}
 // when landing
 loadFromLocalStorage();
 renderTasks(tasks);
 checkForChange();
-
